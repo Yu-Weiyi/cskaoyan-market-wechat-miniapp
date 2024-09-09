fb34abb1b5ed70cd8431af009fc435664cd1a0f4
WaYease <yu_weiyi@outlook.com>
2024-09-09 18:08:30 +0800

    feat: replace banner

--- src/main/java/happy/coding/controller/HealthCheckController.java
+++ src/main/java/happy/coding/controller/HealthCheckController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
